30d1e0acbcdfce4294fa5c578c857c19560ccd1d
birbbbbbbie <116534973+birbbbbbbie@users.noreply.github.com>
2025-01-14 07:21:13 -0800

    added swerve code and projected states

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -13,14 +16,29 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.Superstructure.SSStates;
+import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
+import frc.robot.subsystems.swerve.TunerConstants;
+import static edu.wpi.first.units.Units.*;
 
 public class RobotContainer {
   private final CommandXboxController driver = new CommandXboxController(0); // My joystick
   private final CommandXboxController operator = new CommandXboxController(1);
 
+  private final TunerConstants tunerConst = new TunerConstants();
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
   Superstructure superstructure = new Superstructure();
 
   // ------- Swerve Generated -------
+  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+          .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+          .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
   public SendableChooser<Command> autonChooser = new SendableChooser<Command>();
 
@@ -47,6 +65,23 @@ public class RobotContainer {
 
   public void configureBindings() {
     // --------------------=Driver=--------------------
+
+    drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+    drivetrain.applyRequest(() ->
+        drive.withVelocityX(-driver.getLeftY() * MaxSpeed * 0.4) // Drive forward with negative Y (forward)
+            .withVelocityY(-driver.getLeftX() * MaxSpeed * 0.4) // Drive left with negative X (left)
+            .withRotationalRate(-driver.getRightX() * MaxAngularRate * 0.6) // Drive counterclockwise with negative X (left)
+        )
+    );
+
+    driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    driver.b().whileTrue(drivetrain.applyRequest(() ->
+        point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))
+    ));
+
+    // reset the field-centric heading on left bumper press
+    driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
     // --------------------=Operator=--------------------
 
@@ -77,8 +112,4 @@ public class RobotContainer {
       )
     );
   }
-
-  // public Command alignSwerveCommand() {
-  //   return drivetrain.applyRequest(() -> headingLock.withSpeeds(vision.getHeadingLockSpeed()));
-  // }
 }
--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.RobotConfig;
+
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,12 +14,33 @@ public class Superstructure extends SubsystemBase {
 
   public static enum SSStates {
     NONE,
-    STOWED
+    STOWED,
+    INTAKE,
+    HANDOFF,
+    CORAL_1,
+    CORAL_2,
+    CORAL_3,
+    ALGAE_REMOVE_2,
+    ALGAE_REMOVE_3,
+    SHALLOW_CLIMB,
+    DEEP_CLIMB
   }
+
+  /**
+   * funnel top roller intake - can align with 2 thingies - only need to spin wheels
+   * fixed angle top and bottom outtake - to remove algae just bump into reef and spin wheels out then back away - just spin wheels for outtake
+   * 28 by 28 in swerve drivebase
+   * subsystems: intake (rollers mayb beam break or some sort of detection)(Emily), elevator (TBD)(Jai), shooter (rollers)(Bella), vision (Zack, Aaron, Eric), pathplanner (Jai, Bella, Emily), superstructure (Denielle)
+   */
 
   public SSStates currentState = SSStates.NONE;
   public SSStates lastState = SSStates.NONE;
   public SSStates wantedState = SSStates.NONE;
+
+  RobotConfig robotConfig = new RobotConfig(1.0, 4.5, null, null);
+  ModuleConfig moduleConfig = new ModuleConfig(null, null, 0, null, null, 0);
+  DCMotor dcMotor = new DCMotor(0, 0, 0, 0, 0, 0);
+  TalonFX motor = new TalonFX(0);
 
   private Timer timer = new Timer();
 
@@ -34,13 +59,99 @@ public class Superstructure extends SubsystemBase {
     currentState = wantedState;
 
     switch (currentState) {
+      case NONE:
+        break;
 
+      case STOWED:
+      default:
+        stowed();
+        break;
+
+      case INTAKE:
+        intake();
+        break;
+
+      case HANDOFF:
+        handoff();
+        break;
+
+      case CORAL_1:
+        coral1();
+        break;
+
+      case CORAL_2:
+        coral2();
+        break;
+
+      case CORAL_3:
+        coral3();
+        break;
+
+      case ALGAE_REMOVE_2:
+        algaeRemove2();
+        break;
+
+      case ALGAE_REMOVE_3:
+        algaeRemove3();
+        break;
+
+      case DEEP_CLIMB:
+        deepClimb();
+        break;
     }
   }
 
   // ------ states ------
 
+  private void stowed() {
+    // all subsystems at stowed
+  }
 
+  private void intake() {
+    // roll intake
+    // stop it in robot container
+  }
+
+  private void handoff() {
+    // roll intake
+    // roll shooter
+    // stop both in robot container
+  }
+
+  private void coral1() {
+    // move elevator to L1
+    // claw release will be seperate (similar to shooting last year)
+  }
+
+  private void coral2() {
+    // move elevator to L2
+    // swerve alignment might have to separate in robot container
+    // claw release will be seperate (similar to shooting last year)
+  }
+
+  private void coral3() {
+    // move elevator to L3
+    // turn pivot to correct angle
+    // swerve alignment might have to separate in robot container
+    // claw release will be seperate (similar to shooting last year)
+  }
+
+  private void algaeRemove2() {
+    // move elevator to L2
+    // turn wheels
+    // may have to retract elevator not sure tbh
+  }
+
+  private void algaeRemove3() {
+    // move elevator to L2
+    // turn wheels
+    // may have to retract elevator not sure tbh
+  }
+
+  private void deepClimb() {
+    // ig align as best as it can
+    // lowk might leave it as manual
+  }
 
   // ------ commands -------
   /**
