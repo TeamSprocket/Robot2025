7a3d8533e149088a929fd51f61ed84dca2b48d54
Emily Luo <emily0127coding@gmail.com>
2025-07-24 20:16:29 -0700

    Subsystem Test on 07/24/2025

--- src/main/java/frc/robot/subsystems/Subsystem.java
+++ src/main/java/frc/robot/subsystems/Subsystem.java
@@ -22,16 +22,83 @@ import frc.util.Util;
 import frc.util.Alert;
 
 public class Subsystem extends SubsystemBase {
-    public static enum SubsystemStates {
-        NONE
+    private final TalonFX elevatormotor1 = new TalonFX(0);
+    private final TalonFX elevatormotor2 = new TalonFX(1);
+
+    private final MotionMagicVoltage motionmagic = new MotionMagicVoltage(0.0);
+
+    //PIDSVAG
+
+    public void motorconfigs(){
+        TalonFXConfiguration configs = new TalonFXConfiguration();
+        configs.withSlot0(
+            new Slot0Configs()
+            .withKP(0)
+            .withKI(0)
+            .withKD(0)
+            .withKS(0)
+            .withKV(0)
+            .withKA(0)
+            .withKG(0)
+            .withGravityType(GravityTypeValue.Elevator_Static)
+
+        );
+        
     }
 
     public Subsystem() {
+    }
 
+    private void gotoheight(int num)
+    {
+        motionmagic = motionmagic.withPosition(num);
+        elevatormotor1.setControl(motionmagic);
+        elevatormotor2.setControl(motionmagic);
+        
+    }
+
+    private SubsystemStates state = SubsystemStates.LOW;
+
+
+    public static enum SubsystemStates {
+        LOW,
+        MEDIUM,
+        HIGH
     }
 
     @Override
     public void periodic() {
+        switch (state)
+        {
+            case LOW:
+                gotoheight(0);
+
+                break;
+            case MEDIUM:
+                gotoheight(1);
+                break;
+            case HIGH:
+                gotoheight(2);
+                break;
+
+        }
 
     }
+
+
+    //
+    public SubsystemStates settheState(SubsystemStates setstate)
+    {
+        this.state=setstate;
+        return state;
+    }
+
+
+    public SubsystemStates gettheState()
+    {
+        return state;
+    }
+
+
+
 }
